9424bca073dc66cd267f62d0bbea59260abf3bec
Egor <lldocll2012@outlook.com>
2023-07-19 12:16:19 +0300

    решили задачу

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.Clear();
+int Input(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+int x = Input("Введите x ");
+int y = Input("Введите y ");
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка пренадлежит первой четверти ");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Точка пренадлежит второй четверти ");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Точка пренадлежит третьей четверти ");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Точка пренадлежит четвертой четверти ");
+}
+else
+{
+    Console.WriteLine("Точка лежит на оси");
+}
