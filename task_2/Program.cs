65233e79b7626d4a54fdb9ae035ec68dbde2aa83
Egor <lldocll2012@outlook.com>
2023-07-19 12:27:29 +0300

    решение второй задачи

---
+++ task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая по заданному номеру четверти , показывает диапозон возможных координат точек в этой четверти.
+
+Console.Clear();
+int Input(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+int num = Input("Введите номер четверти");
+
+if(num == 1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+} 
+else if(num == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if(num == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if(num == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
