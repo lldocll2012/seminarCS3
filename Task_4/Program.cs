d24d1b999e7e16b8187fa2be70a820f002023dd6
Egor <lldocll2012@outlook.com>
2023-07-19 17:09:14 +0300

    make it

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая 
+принимает на вход число (N) и выдаёт таблицу 
+квадратов чисел от 1 до N
+*/
+
+Console.Clear();
+int Input(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int a = Input("Pleas enter the number ");
+int count = 1;
+while(count <= a)
+{
+    Console.WriteLine(count * count);
+    count++;
+}
